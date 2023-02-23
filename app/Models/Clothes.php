4ecd76d5033e87ffeee0620fe7dd561cc8daa595
manuelsvfz <manuel.silva@ibadia.cat>
2023-02-23 16:01:33 +0100

    first commit

---
+++ app/Models/Clothes.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Clothes extends Model
+{
+    protected $fillable = ['clotheType_id', 'size',  'color', 'price', 'gender'];
+
+    public function clotheType()
+    {
+        return $this->hasOne(ClothesType::class);
+    }
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,82 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::create('clothes_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('discounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('percentageDiscount');
+            $table->timestamps();
+        });
+
+        Schema::create('clothes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('clotheType_id');
+            $table->foreign('clotheType_id')->references('id')->on('clothes_types');
+            $table->string('size');
+            $table->string('color');
+            $table->decimal('price');
+            $table->string('gender');
+            $table->timestamps();
+        });
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->boolean('isAdmin');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('bank_data', function (Blueprint $table) {
+            $table->id();
+            $table->string('iban');
+            $table->decimal('money');
+            $table->timestamps();
+        });
+
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('favorites_id');
+            $table->foreign('favorites_id')->references('id')->on('clothes');
+            $table->unsignedBigInteger('shoppinCart_id');
+            $table->foreign('shoppinCart_id')->references('id')->on('clothes');
+            $table->unsignedBigInteger('bankData_id');
+            $table->foreign('bankData_id')->references('id')->on('bank_data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('clothes');
+        Schema::dropIfExists('admins');
+        Schema::dropIfExists('clients');
+        Schema::dropIfExists('discounts');
+        Schema::dropIfExists('clothes_types');
+        Schema::dropIfExists('bank_data');
+    }
+};
